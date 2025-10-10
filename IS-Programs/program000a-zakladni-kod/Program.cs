17276ec3bf56442c8613308725c8365f8e91549d
Amálie Musilová <amusilova222@gmail.com>
2025-10-10 15:41:33 +0200

    10/10 commit

---
+++ IS-Programs/program000a-zakladni-kod/Program.cs
@@ -0,0 +1,32 @@
+string again = "a";
+while (again == "a")
+{
+    Console.Clear();
+    Console.WriteLine("****************************");
+    Console.WriteLine("******Výpis řady čísel******");
+    Console.WriteLine("****************************");
+    Console.WriteLine("******Amálie Musilová*******");
+    Console.WriteLine("****************************");
+    Console.WriteLine();
+
+    // vstup hodnoty do programu - špatně řešený 
+    //Console.Write("Zadejte první číslo řady: ");
+    //int first = int.Parse(Console.ReadLine());
+
+    // vstup hodnot do programu - řešený správně
+    Console.Write("Zadejte první číslo řady (celé číslo): ");
+    int first;
+
+    while (!int.TryParse(Console.ReadLine(), out first))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte první číslo řady znovu: "); 
+}
+
+
+    Console.WriteLine();
+    Console.WriteLine("Pro opakování programu stiskněte klávesu 'a'.");
+    again = Console.ReadLine();
+
+
+
+}
